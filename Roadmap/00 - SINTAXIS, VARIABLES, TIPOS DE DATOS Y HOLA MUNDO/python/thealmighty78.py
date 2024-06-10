b5a6ee8e4e2f57ae97259c0f200b50ceefed67cc
thealmighty78 <jorgesantaella@gmail.com>
2024-06-10 22:23:08 +0200

    Create thealmighty78.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/thealmighty78.py
@@ -0,0 +1,26 @@
+# https://python.org/
+
+# Comentario en una linea
+
+""" ==Docstring
+Aqui añado
+comentarios en 
+varias lineas
+"""
+
+'''  creo que con comillas simples tambien se llama docstring
+Por  lo que de esta menera 
+tambien es una opción valida
+de añadir comentarios
+en varias lineas
+'''
+
+lenguaje = 'python'
+
+cadenas_de_texto = 'Esta es una cadena de texto'
+enteros = 10
+numeros_decimales = 1.50
+booleanos = True
+
+print(f"!Hola, {lenguaje.title()}!")
+
